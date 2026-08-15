da7db46a3c91878f35f1175cecb1acb29802b7f5
HadinNazmi <hadintheking@gmail.com>
2026-08-15 13:59:49 +0700

    Donasi Controller dan sudah di uji dengan foto

--- jelantah-backend/routes/api.php
+++ jelantah-backend/routes/api.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\LokasiController;
+use App\Http\Controllers\Api\DonasiController;
+
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
@@ -16,12 +18,19 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::middleware('role:donatur')->group(function () {
         Route::get('/lokasi', [LokasiController::class, 'index']);
         Route::get('/lokasi/{id}', [LokasiController::class, 'show']);
+        Route::post('/donasi', [DonasiController::class, 'store']);
+        Route::get('/donasi', [DonasiController::class, 'myDonasi']);
+        Route::get('/donasi/{id}', [DonasiController::class, 'show']);
     });
 
     // Pengelola
     Route::middleware('role:pengelola')->group(function () {
         Route::get('/pengelola/lokasi', [LokasiController::class, 'myLokasi']);
         Route::put('/pengelola/lokasi/{id}', [LokasiController::class, 'updateJadwal']);
+        Route::get('/pengelola/donasi', [DonasiController::class, 'incomingDonasi']);
+        Route::put('/pengelola/donasi/{id}/verifikasi', [DonasiController::class, 'verifikasi']);
+        Route::put('/pengelola/donasi/{id}/selesai', [DonasiController::class, 'selesai']);
+        Route::get('/pengelola/rekap', [DonasiController::class, 'rekapLokasi']);
     });
 
     // Manajemen
@@ -30,5 +39,6 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::post('/manajemen/lokasi', [LokasiController::class, 'store']);
         Route::put('/manajemen/lokasi/{id}', [LokasiController::class, 'update']);
         Route::delete('/manajemen/lokasi/{id}', [LokasiController::class, 'destroy']);
+        Route::get('/manajemen/dashboard', [DonasiController::class, 'dashboardAgregat']);
     });
 });
